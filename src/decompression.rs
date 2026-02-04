06b03f718bb458b408d972fc5f3c97ee31770f44
Markus Stange <mstange.moz@gmail.com>
2026-02-04 15:10:47 -0500

    Improve buffer management for decompression.

--- src/decompression.rs
+++ src/decompression.rs
@@ -3,6 +3,7 @@ use zstd_safe::{DCtx, InBuffer, OutBuffer};
 /// A zstd decompressor for PERF_RECORD_COMPRESSED records.
 pub struct ZstdDecompressor {
     dctx: Option<DCtx<'static>>,
+    buf: Option<Vec<u8>>,
 }
 
 impl Default for ZstdDecompressor {
@@ -13,26 +14,32 @@ impl Default for ZstdDecompressor {
 
 impl ZstdDecompressor {
     pub fn new() -> Self {
-        Self { dctx: None }
+        Self {
+            dctx: None,
+            buf: None,
+        }
     }
 
-    /// Decompress a chunk of zstd data.
-    pub fn decompress(&mut self, compressed_data: &[u8]) -> Result<Vec<u8>, std::io::Error> {
+    /// Decompress a chunk of zstd data and append it to `uncompressed_data`.
+    pub fn decompress_into(
+        &mut self,
+        compressed_data: &[u8],
+        uncompressed_data: &mut Vec<u8>,
+    ) -> Result<(), std::io::Error> {
         let dctx = self.dctx.get_or_insert_with(DCtx::create);
+        let buf = self.buf.get_or_insert_with(|| vec![0; 1024 * 1024]);
 
-        let mut decompressed = vec![0; compressed_data.len() * 4];
         let mut in_buffer = InBuffer::around(compressed_data);
-        let mut total_out = 0;
 
-        while in_buffer.pos < in_buffer.src.len() {
-            let available = decompressed.len() - total_out;
-            let mut out_buffer = OutBuffer::around(&mut decompressed[total_out..]);
+        loop {
+            let mut out_buffer = OutBuffer::around(&mut buf[..]);
 
             match dctx.decompress_stream(&mut out_buffer, &mut in_buffer) {
                 Ok(_) => {
-                    total_out += out_buffer.pos();
-                    if out_buffer.pos() == available {
-                        decompressed.resize(decompressed.len() + compressed_data.len() * 4, 0);
+                    let out_len = out_buffer.pos();
+                    uncompressed_data.extend_from_slice(&buf[..out_len]);
+                    if out_len < buf.len() && in_buffer.pos() == compressed_data.len() {
+                        break;
                     }
                 }
                 Err(code) => {
@@ -44,9 +51,6 @@ impl ZstdDecompressor {
                 }
             }
         }
-
-        decompressed.truncate(total_out);
-
-        Ok(decompressed)
+        Ok(())
     }
 }
